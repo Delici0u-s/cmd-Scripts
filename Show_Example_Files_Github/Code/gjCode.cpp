7457bc3ed9b4af95c8bfca6e3edba53e9cce986b
Delici0u-s <maxi.bravo111@gmail.com>
2025-01-11 12:42:38 +0100

    updated gj

--- Show_Example_Files_Github/Code/gjCode.cpp
+++ Show_Example_Files_Github/Code/gjCode.cpp
@@ -1,11 +1,9 @@
-#include <cstdlib>
 #include <filesystem>
-#include <format>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-  if (argc == 1 || (std::string(argv[1]) ==  "--help") )
+  if (argc == 1 || (std::string(argv[1]) == "--help"))
   {
     std::cout << "Usage: " << argv[0] << " And then all items to be removed.\n:";
     std::cout << "Example: \n";
@@ -14,33 +12,17 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  unsigned int TotRemItems{0};
+  unsigned int count{0};
   for (int i{1}; i < argc; ++i)
   {
-    // Check if the file/directory exists, then remove accordingly with user feedback if file doesnt exist
-    if (std::filesystem::exists(argv[i]))
+    if (std::filesystem::remove_all(argv[i]) == 0)
     {
-      int success{system(std::format("rm -f -r -- {}", argv[i]).c_str())};
-      if (success)
-      {
-        std::cout << argv[i] << " failed execute command on this item\n";
-      }
-      else if (std::filesystem::exists(argv[i]))
-      {
-        std::cout << "failed to remove: " << argv[i] << '\n';
-      }
-      else
-      {
-        ++TotRemItems;
-      }
+      std::cout << '"' << argv[i] << '"' << " could not be removed\n";
     }
     else
     {
-      std::cout << argv[i] << " does not exist\n";
+      ++count;
     }
   }
-
-  std::cout << TotRemItems << " out of " << argc - 1 << " items successfully removed\n";
-
-  return 0;
+  std::cout << count << " out of " << argc - 1 << " items successfully removed\n";
 }
